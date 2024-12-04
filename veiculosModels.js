0f48e425aa2b3042b219603dba58935897be2800
vitor alexandre <165023350+mercenarios13@users.noreply.github.com>
2024-12-04 09:27:47 -0300

    Create veiculosModels.js

---
+++ veiculosModels.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const veiculoSchema = new mongoose.Schema({
+  modelo: {
+    type: String,
+    required: true, 
+  },
+  marca: {
+    type: String,
+    required: true, 
+  },
+  ano: {
+    type: Number,
+    required: true, 
+  },
+  cor: {
+    type: String,
+    required: true, 
+  },
+});
+
+const Veiculo = mongoose.model('Veiculo', veiculoSchema);
+
+module.exports = Veiculo; 
