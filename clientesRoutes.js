cd16b44d88251b93772980f923c96b4aaab2f0c3
vitor alexandre <165023350+mercenarios13@users.noreply.github.com>
2024-12-04 09:56:03 -0300

    Update clientesRoutes.js

--- clientesRoutes.js
+++ clientesRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const clienteController = require('../controllers/clienteController');  
+const router = express.Router();
 
+router.get('/clientes', clienteController.listarClientes);
+
+router.post('/clientes', clienteController.criarCliente);
+
+router.put('/clientes/:id', clienteController.atualizarCliente);
+
+router.delete('/clientes/:id', clienteController.excluirCliente);
+
+module.exports = router;
